efc18e81884444b7014c8dd7bf25559255eea8f6
Sujatha7981996839 <sujatha.nimeesha@gmail.com>
2024-08-10 13:36:51 +0000

    solution

---
+++ src/main/java/com/example/nxtstayz/controller/RoomController.java
@@ -0,0 +1,47 @@
+package com.example.nxtstayz.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+
+import com.example.nxtstayz.model.*;
+import com.example.nxtstayz.service.RoomJpaService;
+
+// Write your code here
+@RestController
+public class RoomController {
+
+    @Autowired
+    private RoomJpaService roomJpaService;
+
+    @GetMapping("/hotels/rooms")
+    public ArrayList<Room> getRooms() {
+        return roomJpaService.getRooms();
+    }
+
+    @GetMapping("/hotels/rooms/{roomId}")
+    public Room getRoomById(@PathVariable("roomId") int roomId) {
+        return roomJpaService.getRoomById(roomId);
+    }
+
+    @PostMapping("/hotels/rooms")
+    public Room addRoom(@RequestBody Room room) {
+        return roomJpaService.addRoom(room);
+    }
+
+    @PutMapping("/hotels/rooms/{roomId}")
+    public Room updateRoom(@PathVariable("roomId") int roomId, @RequestBody Room room) {
+        return roomJpaService.updateRoom(roomId, room);
+    }
+
+    @DeleteMapping("/hotels/rooms/{roomId}")
+    public void deleteRoom(@PathVariable("roomId```") int roomId) {
+        roomJpaService.deleteRoom(roomId);
+    }
+
+    @GetMapping("/rooms/{roomId}/hotel")
+    public Hotel getRoomOfHotel(@PathVariable("roomId") int roomId) {
+        return roomJpaService.getRoomOfHotel(roomId);
+    }
+
+}
---
+++ src/main/java/com/example/nxtstayz/model/Room.java
@@ -0,0 +1,78 @@
+package com.example.nxtstayz.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "room")
+public class Room {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int roomId;
+
+    @Column(name = "roomNumber")
+    private String roomNumber;
+
+    @Column(name = "type")
+    private String type;
+
+    @Column(name = "price")
+    private double price;
+
+    @ManyToOne
+    @JoinColumn(name = "hotelId")
+    private Hotel hotel;
+
+    public Room() {
+    }
+
+    public Room(int roomId, String roomNumber, String type, double price, Hotel hotel) {
+        this.roomId = roomId;
+        this.roomNumber = roomNumber;
+        this.type = type;
+        this.price = price;
+        this.hotel = hotel;
+
+    }
+
+    public int getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(int roomId) {
+        this.roomId = roomId;
+    }
+
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(String roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public String getRoomType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Hotel getHotel() {
+        return hotel;
+    }
+
+    public void setHotel(Hotel hotel) {
+        this.hotel = hotel;
+    }
+
+}
